c180a7b8a0062e4515669f6fff0f3f83b738a7d4
stephen kihuni <stephenkihuni55@gmail.com>
2023-03-30 03:15:57 +0300

    configured .env file

--- README.md
+++ README.md
@@ -21,10 +21,9 @@
  ## Projects vs. apps
 
  - What’s the difference between a project and an app? An app is a web application that does something – e.g., a blog system, a database of public records or a small poll app. A project is a collection of configuration and apps for a particular website. A project can contain multiple apps. An app can be in multiple projects.
-<<<<<<< HEAD
 
  ## Creating Models
 
  - A model is the single, definitive source of information about your data. It contains the essential fields and behaviors of the data you’re storing
-=======
->>>>>>> f85cfb8d1223ce8fc66a355fcc361807ef064704
+
+ 
--- test_site/test_site/settings.py
+++ test_site/test_site/settings.py
@@ -1,4 +1,8 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oc#6mbuw7-xk7z))c6rb(tc@q&y8b0oj54q8_avi=m^wd=*$rv'
+SECRET_KEY = os.getenv('SKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
