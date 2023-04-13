069e70563d7b7961f4c7781499dccb8f713604fa
stephen kihuni <stephenkihuni55@gmail.com>
2023-04-13 04:03:18 +0300

    added views

--- test_site/polls/urls.py
+++ test_site/polls/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from .import views
 
+# This adds namespace to the application
+app_name = "polls"
+
 urlpatterns =[
     path('', views.index, name='index'),
+
+    # the 'name' value as called by the {% url %} template tag
     path("<int:question_id>/", views.detail, name="detail"),
     path("<int:question_id>/result/", views.result, name='result'),
     path("<int:question_id>/vote/", views.vote, name="vote")
--- test_site/polls/views.py
+++ test_site/polls/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
 from .models import Question
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_test for q in latest_question_list])
-    return HttpResponse(output)
+    ## context is a dictionary mapping template variable names to python objects
+    context = {"latest_question_list": latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at questions %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def result(request, question_id):
     response = "You're looking at the result of questions %s."
-    return HttpResponse(response % question_id)
+    return render(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render("You're voting on question %s." % question_id)
