23f95efd6a912af9bb46a7501e4318763ce05826
stephen kihuni <stephenkihuni55@gmail.com>
2023-04-20 04:07:04 +0300

    modified templates file

--- test_site/polls/urls.py
+++ test_site/polls/urls.py
@@ -10,6 +10,6 @@ urlpatterns =[
 
     # the 'name' value as called by the {% url %} template tag
     path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/result/", views.result, name='result'),
+    path("<int:question_id>/result/", views.results, name='result'),
     path("<int:question_id>/vote/", views.vote, name="vote")
 ]
